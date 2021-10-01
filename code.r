a03494856bb69c42e8267629dbb644ceffd11151
NishuDissanayake <nishudissanayake5252@gmail.com>
2021-10-01 22:03:55 +0530

    loading operation

--- code.r
+++ code.r
@@ -1,8 +1,9 @@
 #Installing all the required packages
 if(require("xlsx") == FALSE) {install.packages("xlsx")};{library(xlsx)}
+if(require("ggplot2") == FALSE) {install.packages("ggplot2")};{library(ggplot2)}
 
 #loading data set
-
+mark_sheet = read.xlsx('dataset.xlsx', sheetIndex = 1)
 
 # viwe data set
 
