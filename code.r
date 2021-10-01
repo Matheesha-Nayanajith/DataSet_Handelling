938866fcbd9ecdeafd720b4892e6b19f378da21a
Matheesha-Nayanajith <nayanajithmatheesha@gmail.com>
2021-10-01 21:56:05 +0530

    update

---
+++ code.r
@@ -0,0 +1,19 @@
+#Installing all the required packages
+if(require("xlsx") == FALSE) {install.packages("xlsx")};{library(xlsx)}
+
+#loading data set
+
+
+# viwe data set
+
+
+#viwe data set one by one
+
+
+#show column set
+
+
+#find mean
+
+
+#find sum
