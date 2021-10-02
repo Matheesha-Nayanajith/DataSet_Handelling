69ba13b8173f5736d51e6ccb6a155745856cbb97
oshan-vishwajith <72697298+oshan-vishwajith@users.noreply.github.com>
2021-10-02 15:07:53 +0530

    finished code.

--- code.r
+++ code.r
@@ -5,20 +5,19 @@ if(require("ggplot2") == FALSE) {install.packages("ggplot2")};{library(ggplot2)}
 #loading data set
 mark_sheet = read.xlsx('dataset.xlsx', sheetIndex = 1)
 
-# viwe data set
+
 mark_sheet$Name
 mark_sheet$C.lang..Marks
 mark_sheet$Java.lang..Marks
 mark_sheet$Python.lang..Marks
 mark_sheet$R.lang..Marks
-
 #viwe data set one by one
 
-
+mark_sheet[mark_sheet$STDID==101,]
 #show column set
 
-
+mean(mark_sheet$Java.lang..Marks)
 #find mean
 
-
+sum(mark_sheet$C.lang..Marks)
 #find sum
